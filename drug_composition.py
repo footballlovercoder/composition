24c894c9db1ea2b502f338ebff950881d35eafa1
coder <38506847+footballlovercoder@users.noreply.github.com>
2023-03-10 21:58:57 +0530

    Add files via upload

---
+++ drug_composition.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 10 21:16:14 2023
+
+@author: ritup
+"""
+import altair as alt
+from datetime import date
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+import regex as re
+import requests
+import math
+from dateutil.relativedelta import relativedelta
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP
+import smtplib
+import sys
+url3='https://raw.githubusercontent.com/footballlovercoder/wellocity_consumption/main/drug_info1.csv'
+url4='https://raw.githubusercontent.com/footballlovercoder/wellocity_consumption/main/drug_info2.csv'
+def clean_data(x):
+    if x[-1]==',':
+        return x[:-1]
+    
+@st.experimental_memo
+def load_data():
+        df1 = pd.read_csv(url3,sep = ',',header=0)
+        df1=df1.drop(['Unnamed: 0'],axis=1)
+        df2=pd.read_csv(url4,sep = ',',header=0)
+        df2=df2.drop(['Unnamed: 0'],axis=1)
+        return df1,df2
+df1,df2= load_data()
+df=pd.concat([df1,df2])
+df=df.fillna(' ')
+df['Salt']=df['Salt'].apply(lambda x:clean_data(x))
+df['Uses']=df['Uses'].apply(lambda x:clean_data(x))
+df['Alternate Medicines']=df['Alternate Medicines'].apply(lambda x:clean_data(x))
+df=df.rename(columns={"Medicine Name":"Item_Name"})
+data=df.copy()
+choice=st.selectbox('Medicine Name',data['Item_Name'].values)
+data_filtered=data[data['Item_Name']==choice]
